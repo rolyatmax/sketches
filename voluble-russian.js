4920f42052a027f5088e5cbb9257b77af4a987d5
Taylor Baldwin <taylorbaldwin@gmail.com>
2016-08-07 16:58:52 -0400

    remove dead code, add tiles, change defaults

--- voluble-russian.js
+++ voluble-russian.js
@@ -1,13 +1,9 @@
 import Alea from 'alea'
-import SimplexNoise from 'simplex-noise'
-// import vec2 from 'gl-vec2'
 import tinycolor from 'tinycolor2'
 import colorPalettes from './common/color-palettes'
 import createProject from './common/create-project'
 
 createProject({
-  // aspectRatio: 9 / 7,
-
   settingsConfig: {
     seed: [0, 999],
     trianglesCount: [0, 1000],
@@ -18,10 +14,10 @@ createProject({
 
   defaultSettings: {
     seed: Math.random() * 1000 | 0,
-    trianglesCount: 2,
-    circleRadius: 5,
-    colorAlpha: 70,
-    circleMargin: 0
+    trianglesCount: 3,
+    circleRadius: 12,
+    colorAlpha: 50,
+    circleMargin: 25
   },
 
   tiles: [
@@ -33,7 +29,8 @@ createProject({
     '643GyreO',
     '64oCBkata',
     '64jDhfI',
-    '64OnTYS'
+    '64OnTYS',
+    '64YwkLMen'
   ],
 
   main: (canvas, settings, scale = 1) => {
@@ -41,6 +38,7 @@ createProject({
     const ctx = window.ctx = canvas.getContext('2d')
 
     ctx.globalCompositeOperation = 'darker'
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     const rand = new Alea(seed)
     // const simplex = window.simplex = new SimplexNoise(rand)
     const palette = colorPalettes[rand() * colorPalettes.length | 0]
@@ -89,3 +87,5 @@ createProject({
     }
   }
 })
+
+// [{"circleMargin":10,"circleRadius":20,"colorAlpha":50,"seed":757,"trianglesCount":10},{"circleMargin":10,"circleRadius":10,"colorAlpha":50,"seed":490,"trianglesCount":10},{"circleMargin":10,"circleRadius":5,"colorAlpha":50,"seed":817,"trianglesCount":1},{"circleMargin":25,"circleRadius":20,"colorAlpha":40,"seed":569,"trianglesCount":10},{"circleMargin":0,"circleRadius":25,"colorAlpha":50,"seed":990,"trianglesCount":3},{"circleMargin":0,"circleRadius":25,"colorAlpha":20,"seed":779,"trianglesCount":50},{"circleMargin":10,"circleRadius":180,"colorAlpha":20,"seed":523,"trianglesCount":40},{"circleMargin":0,"circleRadius":2,"colorAlpha":20,"seed":688,"trianglesCount":10},{"circleMargin":0,"circleRadius":5,"colorAlpha":70,"seed":24,"trianglesCount":2},{"circleMargin":25,"circleRadius":12,"colorAlpha":50,"seed":411,"trianglesCount":3}]
