0c2f1a2fd8182c8acb1ec922c1ae928da1d8a61e
Taylor Baldwin <taylorbaldwin@gmail.com>
2016-10-09 18:14:25 -0400

    updates to corpulent

--- src/corpulent-porcupine.js
+++ src/corpulent-porcupine.js
@@ -1,29 +1,33 @@
 import Sketch from 'sketch-js'
 import array from 'new-array'
-import { random, add, subtract, scale } from 'gl-vec2'
+import { add, subtract, scale } from 'gl-vec2'
 
-const drawersCount = 100
-const lineLength = 30
+const drawersCount = 200
+const lineLength = 10
 const turnDegrees = 60
-const drawSpeed = 0.55
-const lineColor = 'rgba(0, 0, 0, 0.01)'
+const drawSpeed = 0.9
+const lineColor = 'rgba(100, 100, 100, 0.15)'
 
 let drawers
-// let pathsTaken = {}
+let pathsTaken = {}
 
 const ctx = Sketch.create({
   autoclear: false
 })
 
 ctx.setup = function () {
-  const center = [ctx.width / 2, ctx.height / 2]
-  drawers = array(drawersCount).map(() => ({
-    position: add([], random([]), center),
-    progress: 0
-  }))
+  drawers = array(drawersCount).map(createDrawer)
 }
 
 ctx.update = function () {
+  if (!drawers.length) {
+    ctx.stop()
+    return
+  }
+  if (drawers.length < drawersCount && Math.random() < 0.5) {
+    const drawerToClone = drawers[Math.random() * drawers.length | 0]
+    drawers.push(createDrawer(drawerToClone.position))
+  }
   drawers.forEach((drawer) => {
     if (!drawer.nextPosition || drawer.progress === 1) {
       drawer.position = drawer.nextPosition || drawer.position
@@ -33,6 +37,7 @@ ctx.update = function () {
     drawer.progress += (1 - drawer.progress) * drawSpeed
     drawer.progress = (drawer.progress > 0.99) ? 1 : drawer.progress
   })
+  drawers = drawers.filter(drawer => drawer.nextPosition)
 }
 
 ctx.draw = function () {
@@ -48,15 +53,66 @@ ctx.draw = function () {
   })
 }
 
+function createDrawer (position) {
+  const center = [ctx.width / 2, ctx.height / 2]
+  return {
+    position: position || center,
+    progress: 0
+  }
+}
+
 function getNextPoint (curPosition) {
-  const turnCount = 360 / turnDegrees
-  const degs = ((Math.random() * turnCount) | 0) * turnDegrees
-  const rads = degs / 360 * Math.PI * 2
-  const vec = scale([], [Math.cos(rads), Math.sin(rads)], lineLength)
-  const next = add(vec, vec, curPosition)
+  const points = getPossiblePoints(curPosition)
+  if (!points.length) return null
+  let next = points[(Math.random() * points.length) | 0]
+  pathsTaken[key(curPosition, next)] = true
+  if (!isInViewport(next)) return null
+
+  // throw in something fun
+  // if (Math.random() < 0.005) {
+  //   next = next.map(num => num + 1)
+  // }
+
   return next
 }
 
-// function key (position) {
-//   return `${position[0]}-${position[1]}`
-// }
+function getPossiblePoints (position) {
+  const directions = []
+  let t = 0
+  while (t < 360) {
+    directions.push(t)
+    t += turnDegrees
+  }
+  const points = directions.map(dir => {
+    const rads = dir / 360 * Math.PI * 2
+    const vec = scale([], [Math.cos(rads), Math.sin(rads)], lineLength)
+    return add(vec, vec, position)
+  })
+  return points.filter(pt => !pathsTaken[key(pt, position)])
+}
+
+function isInViewport (point) {
+  if (point[0] < 0 || point[1] < 0) return false
+  if (point[0] > ctx.width || point[1] > ctx.height) return false
+  return true
+}
+
+function key (pointA, pointB) {
+  pointA = pointA.map(num => Math.round(num))
+  pointB = pointB.map(num => Math.round(num))
+  let first, second
+  if (pointA[0] < pointB[0]) {
+    first = pointA
+    second = pointB
+  } else if (pointA[0] > pointB[0]) {
+    first = pointB
+    second = pointA
+  } else if (pointA[1] < pointB[1]) {
+    first = pointA
+    second = pointB
+  } else {
+    first = pointB
+    second = pointA
+  }
+  return `(${first[0]}-${first[1]})-(${second[0]}-${second[1]})`
+}
