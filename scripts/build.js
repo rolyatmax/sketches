5e8c2e88282df27af5ec5519167b59d3aada91da
Taylor Baldwin <taylorbaldwin@gmail.com>
2020-06-09 23:27:06 -0400

    update build script

--- scripts/build.js
+++ scripts/build.js
@@ -5,30 +5,42 @@ const rimrafSync = require('rimraf').sync
 const config = require('../config')
 const files = ['index'].concat(config.include)
 
-mkdirpSync('docs')
-
-const err = rimrafSync('docs/*.html')
+const err = rimrafSync('docs')
 if (err) throw new Error(err)
+
+mkdirpSync('docs')
 
 // - follow-up: support --force flag to skip the hash checks
 // - follow-up: support --sketch flag to run the update only for a specific sketch (+ index.html)
 
-const copyResourcesPromise = new Promise((resolve, reject) => {
-  childProcess.exec('cp -r resources docs/', (err) => {
-    if (err) return reject(err)
-    resolve()
+console.log('Copying resources dir')
+exec('cp -r resources docs/')
+  .then(() => {
+    console.log('Building sketches & resizing images')
+    return Promise.all([
+      // loop through with imagemagick and resize all the screenshots
+      ...config.include.map(name => exec(`convert docs/resources/screenshots/${name}.png -resize 800 docs/resources/screenshots/${name}.png`)),
+      // build all the sketches
+      ...files.map(name => exec(`canvas-sketch sketches/${name}.js --dir docs --build --inline`))
+    ])
   })
-})
-
-Promise.all([copyResourcesPromise].concat(files.map(buildSketches)))
   .catch((e) => console.error(e))
-  .then(() => console.log('Finished'))
+  .then(() => console.log('Finished!'))
 
-function buildSketches (filename) {
+function exec (cmd) {
   return new Promise((resolve, reject) => {
-    console.log('Building', filename)
-    childProcess.exec(`canvas-sketch sketches/${filename}.js --dir docs --build --inline`, (err) => {
-      if (err) return reject(err)
+    childProcess.exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        console.error('exec error:', err)
+        reject(err)
+        return
+      }
+      if (stdout) {
+        console.error('stdout:', stdout)
+      }
+      if (stderr) {
+        console.error('stderr:', stderr)
+      }
       resolve()
     })
   })
